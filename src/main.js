7543dab29b504c7749da7a9ccf825644927adace
hamid-JTG <hamid.husain@joshtechnologygroup.com>
2025-04-07 19:39:33 +0530

    UI basic

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Swiper from 'swiper'
+import 'swiper/scss'
+
+import './styles/style.scss'
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  grabCursor: true,
+
+  navigation: {
+    nextEl: '.next-button',
+    prevEl: '.prev-button'
+  }
+})
